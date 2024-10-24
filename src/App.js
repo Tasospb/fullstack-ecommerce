c78d1816e39b04422b02650b105f60892b4d57b5
tasospb <107421592+Tasospb@users.noreply.github.com>
2024-10-24 20:51:39 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-danger">Ecommerce website</h1>
-    </>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" exact={true} element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
