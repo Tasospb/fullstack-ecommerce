c0c9cd29603478c815043ced0bc8e2954bb48053
tasospb <107421592+Tasospb@users.noreply.github.com>
2024-11-20 17:07:22 +0000

    main
    changed the dropdown for the countries

--- src/Components/CountryDropDown/index.js
+++ src/Components/CountryDropDown/index.js
@@ -1,5 +1,11 @@
 import Button from '@mui/material/Button';
 import { FaAngleDown } from 'react-icons/fa';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import { IoIosSearch } from "react-icons/io";
+
+
+
 
 const CountryDropDown=()=>{
     return(
@@ -13,7 +19,49 @@ const CountryDropDown=()=>{
                 <FaAngleDown></FaAngleDown>
             </span>
         </Button>
+        <Dialog open={true} className='locationModel'>
+            <h4>Choose your Delivery Location</h4>
+            <p>Enter your address and we will specify the offer for your area.</p>
+                <div className='headerSearch w-100'>
+                <input type='text' placeholder='Search your area...' />
+            <Button><IoIosSearch /></Button>
+             </div>
+
+            <ul className='countryList mt-3'>
+                <li><Button>United Kingdom</Button></li>
+                <li><Button>Austria</Button></li>
+                <li><Button>Belgium</Button></li>
+                <li><Button>Bulgaria</Button></li>
+                <li><Button>Croatia</Button></li>
+                <li><Button>Cyprus</Button></li>
+                <li><Button>Czech Republic</Button></li>
+                <li><Button>Denmark</Button></li>
+                <li><Button>Estonia</Button></li>
+                <li><Button>Finland</Button></li>
+                <li><Button>France</Button></li>
+                <li><Button>Germany</Button></li>
+                <li><Button>Greece</Button></li>
+                <li><Button>Hungary</Button></li>
+                <li><Button>Ireland</Button></li>
+                <li><Button>Italy</Button></li>
+                <li><Button>Latvia</Button></li>
+                <li><Button>Lithuania</Button></li>
+                <li><Button>Luxembourg</Button></li>
+                <li><Button>Malta</Button></li>
+                <li><Button>Netherlands</Button></li>
+                <li><Button>Poland</Button></li>
+                <li><Button>Portugal</Button></li>
+                <li><Button>Romania</Button></li>
+                <li><Button>Slovakia</Button></li>
+                <li><Button>Spain</Button></li>
+                <li><Button>Sweden</Button></li>
+            </ul>  
+
+        </Dialog>
+
         </>
+
+        
     )
 }
 
--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -9,7 +9,7 @@ const Navigation = ()=>{
         <nav>
         <div className='container'>
             <div className='row'>
-                <div className='col-sm-3 navPart1'>
+                <div className='col-sm-2 navPart1'>
                     <Button className='allCatTab align-items-center'>
                         <span className='icon1 mr-2'><IoIosMenu/></span>
                         <span class="text">ALL CATEGORIES</span>
@@ -17,7 +17,7 @@ const Navigation = ()=>{
                     </Button>
                 </div>
 
-                <div className='col-sm-9 navPart2 d-flex align-items-center'>
+                <div className='col-sm-10 navPart2 d-flex align-items-center'>
                     <ul className='list list-inline ml-auto'>
                         <li className='list-inline-item'><Link to="/">Home</Link></li>
                         <li className='list-inline-item'><Link to="/">T-Shirts</Link></li>
@@ -25,6 +25,7 @@ const Navigation = ()=>{
                         <li className='list-inline-item'><Link to="/">Posters</Link></li>
                         <li className='list-inline-item'><Link to="/">Home Decor</Link></li>
                         <li className='list-inline-item'><Link to="/">Accessories</Link></li>
+                        <li className='list-inline-item'><Link to="/">Contact Us</Link></li>
                         <li className='list-inline-item'><Link to="/">Contact Us</Link></li>
                     </ul>
                 </div>
